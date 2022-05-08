de8aa4771d5b894753a1fde4d8b8389ec7ad2de4
felixcapuano <felix.capuano@gmail.com>
2022-05-08 22:53:18 +0200

    commands

--- app/front/src/Terminal.jsx
+++ app/front/src/Terminal.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useRef } from 'react';
 import $ from 'jquery';
 import 'jquery.terminal';
 import 'jquery.terminal/css/jquery.terminal.min.css';
+import { commands } from './commands';
 
 const Terminal = () => {
   const terminalNode = useRef();
   useEffect(() => {
-    const terminal = $(terminalNode.current).terminal();
+    const terminal = $(terminalNode.current).terminal(commands);
     terminal.clear();
     // console.log(terminal);
     return () => terminal.destroy();
---
+++ app/front/src/commands.js
@@ -0,0 +1,16 @@
+// import {} from 'crud';
+
+export const commands = {
+  ls: () => {
+    return 'ls';
+  },
+  touch: () => {
+    return 'touch';
+  },
+  mkdir: () => {
+    return 'mkdir';
+  },
+  cd: () => {
+    return 'cd';
+  },
+};
